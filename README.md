894fa5097ad8f18968932acf09d73b55f0600ca9
codesharedot <codeto@sent.com>
2019-09-22 02:45:58 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# prizm price 
+# moac price 
 
-Gets the prizm price with Python (from JSON).
+Gets the moac price with Python (from JSON).
 
 https://pythonbasics.org
--- example.py
+++ example.py
@@ -7,8 +7,8 @@ c = CurrencyRates()
 rate = c.get_rate('USD', 'EUR') 
 print(rate)
 
-prizm_api_url = 'https://api.coinmarketcap.com/v1/ticker/prizm/'
-response = requests.get(prizm_api_url)
+moac_api_url = 'https://api.coinmarketcap.com/v1/ticker/moac/'
+response = requests.get(moac_api_url)
 response_json = response.json()
 print(response_json)
 
